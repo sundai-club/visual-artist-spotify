9970159c447785a19f0c30e2dccc18db217f64f9
leaps <lmtejedor@gmail.com>
2024-04-28 23:17:11 -0400

    populate file

---
+++ src/app/[template_slug]/[model_id]/page.tsx
@@ -0,0 +1,8 @@
+export default async function Model({ params }: { params: any }) {
+
+    return (
+      <main className="overflow-hidden w-full h-screen relative flex">
+        <h1>Model to generate images</h1>
+      </main>
+    );
+  }
