b4b9249069643bc16da19f498d54b473f9375785
leaps <lmtejedor@gmail.com>
2024-04-28 21:44:20 -0400

    Update homepage text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,9 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          Use your favorite artists' work ethically
         </h1>
+        <h2 className="text-3xl">Generative Spotify for visual artists</h2>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
